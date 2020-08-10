04093741a5e3aabafb3776f88948d05f29ab3e5a
Egor14 <egorgrekhnev@mail.ru>
2020-08-10 15:11:44 +0300

    initial

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/rates/')
+def get_rates():
+    fsym = request.args.get('fsym')
+    tsyms = request.args.get('tsyms').split(',')
+
+    response = {tsym: convert(fsym, tsym) for tsym in tsyms}
+
+    return jsonify(response)
+
+
+def convert(fsym, tsym):
+    duc_usd_price = 0.06
+    ducx_usd_price = 0.5
+
+    if fsym == 'USD' and tsym == 'DUC':
+        amount = 1 / duc_usd_price
+    elif fsym == 'USD' and tsym == 'DUCX':
+        amount = 1 / ducx_usd_price
+    elif fsym == 'DUC' and tsym == 'USD':
+        amount = duc_usd_price
+    elif fsym == 'DUCX' and tsym == 'USD':
+        amount = ducx_usd_price
+    else:
+        amount = 1
+
+    return amount
